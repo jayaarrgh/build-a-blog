e6f923f6b2b4a562150f81f64d7d9c5e6bde2f96
Jayaarrgh <jrrdmb@umsl.edu>
2017-02-11 12:03:55 -0600

    slightly alterated asciichan

---
+++ main.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import webapp2
+import jinja2
+import os
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+class Blog(db.Model):
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+
+class MainPage(Handler):
+    def render_front(self, subject="", content="", error=""):
+        blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC LIMIT 5")
+        self.render("front.html", subject=subject, content=content, error=error, blogs=blogs)
+
+    def get(self):
+        self.render_front()
+
+    def post(self):
+        subject = self.request.get("subject")
+        content = self.request.get("content")
+
+        if subject and content:
+            a = Blog(subject=subject, content=content)
+            a.put()
+
+            self.redirect("/")
+        else:
+            error = "We need both a subject and some content!"
+            self.render_front(subject, content, error)
+
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+
+# ("/postpage", PostPage)  IDK.....
+
+
+
+
